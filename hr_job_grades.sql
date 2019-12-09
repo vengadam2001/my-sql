6f58ab3aa04848b435a7fcdb958ad8a884821235
Karan <karanvknarayanan@gmail.com>
2019-12-09 14:11:12 +0530

    Compelte database

---
+++ hr_job_grades.sql
@@ -0,0 +1,6 @@
+insert into hr.job_grades (Grade_level, Lowest_sal, Highest_sal) values ('A', 1000, 2999);
+insert into hr.job_grades (Grade_level, Lowest_sal, Highest_sal) values ('B', 3000, 5999);
+insert into hr.job_grades (Grade_level, Lowest_sal, Highest_sal) values ('C', 6000, 9999);
+insert into hr.job_grades (Grade_level, Lowest_sal, Highest_sal) values ('D', 10000, 14999);
+insert into hr.job_grades (Grade_level, Lowest_sal, Highest_sal) values ('E', 15000, 24999);
+insert into hr.job_grades (Grade_level, Lowest_sal, Highest_sal) values ('F', 25000, 40000);
---
+++ hr_regions.sql
@@ -0,0 +1,4 @@
+insert into hr.regions (region_id, region_name) values (1, 'Europe');
+insert into hr.regions (region_id, region_name) values (2, 'Americas');
+insert into hr.regions (region_id, region_name) values (3, 'Asia');
+insert into hr.regions (region_id, region_name) values (4, 'Middle East and Africa');
